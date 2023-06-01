3f696615216bd614d43e1922095d2fb790976800
bab3642 <bbperc1@gmail.com>
2023-06-01 21:15:50 +0000

    add exercises for Hashes chapter

---
+++ Hashes/exercises.rb
@@ -0,0 +1,78 @@
+#1
+
+family = {  uncles: ["bob", "joe", "steve"],
+            sisters: ["jane", "jill", "beth"],
+            brothers: ["frank","rob","david"],
+            aunts: ["mary","sally","susan"]
+          }
+          
+family_array = family.select do |k, v| 
+      k == :brothers || k == :sisters
+  end
+
+arr = family_array.values.flatten
+p arr
+#2 : they both return the same thing except .merge is not destructive (does not modify the original value) while .merge! is. 
+
+hash1 = {
+  price: 200,
+  cost: 300,
+  profit: 100
+}
+
+hash2 = {
+  cost: 350,
+  profit: 150
+}
+
+returned_hash1 = hash1.merge(hash2)
+p hash1
+p returned_hash1
+
+returned_hash2 = hash1.merge!(hash2)
+p hash1
+p returned_hash2
+
+#3
+pets = {
+  cats: ["Jake", "Mr. Meowsington"],
+  dogs: ["Fido", "Lucy", "Bella", "Thor"],
+  snakes: ["Slimy", "Fred"]
+}
+
+puts pets.keys
+puts pets.values
+
+pets.each_pair do |k, v|
+  puts k.to_s.upcase + ":"
+  puts v 
+end 
+
+#4
+person = {name: 'Bob', occupation: 'web developer', hobbies: 'painting'}
+
+puts person[:name]
+
+#5
+hash = {
+  a: 100,
+  b: 200,
+  c: 300,
+  d: 100,
+  e: 200
+}
+
+if (hash.select! { |k, v| v == 15050 })
+  v += 1
+  puts "Affirmative"
+else
+  puts "Negative"
+end
+
+#Answer: # value?
+if hash.value?(100)
+  puts "Got it!"
+else
+  puts "Nope!"
+end
+
